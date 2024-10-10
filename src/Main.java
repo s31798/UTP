c7bb296669cec52946d28e3ec35b91afa9889d18
s25454 <s25454@pjwstk.edu.pl>
2024-10-10 16:54:34 +0200

    changed parameters 8 4

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,6 @@ public class Main {
         Adder adder = new Adder();
         System.out.println(adder.add(1, 2));
         Subtractor subtractor = new Subtractor();
-        System.out.println(subtractor.subtract(6, 3));
+        System.out.println(subtractor.subtract(8, 4));
     }
 }
